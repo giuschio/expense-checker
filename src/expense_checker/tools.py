f76a96779bfc97c6430a8856cf9a09d8618190e4
Giulio Schiavi <61427904+giuschio@users.noreply.github.com>
2023-12-27 17:04:29 +0100

    Feature: interactive category assignment and visualization

--- src/expense_checker/tools.py
+++ src/expense_checker/tools.py
@@ -1,2 +1,124 @@
-def add(a, b):
-    return a+b
+import os
+import json
+import pandas as pd
+
+class ExcelFileReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_and_filter_data(self):
+        df = pd.read_excel(self.file_path)
+        # Remove commas from the description
+        df['description'] = df['description'].astype(str).str.replace(',', '')
+        # Filter out rows with missing amounts
+        return df.dropna(subset=['amount'])
+
+
+class CategoryManager:
+    def __init__(self, description_to_category=None):
+        # categories is a dictionary of description -> category
+        if description_to_category is not None:
+            unique_categories = list(set([category for category in description_to_category.values()]))
+            self.categories = {str(idx): cat for idx, cat in enumerate(unique_categories)}
+            self.description_to_category = description_to_category
+        else:
+            self.categories = {}
+            self.description_to_category = {}
+
+    def get_category_for_description(self, description):
+        if description not in self.description_to_category:
+            self.query_user_for_category(description)
+        return self.description_to_category[description]
+
+    def query_user_for_category(self, description):
+        while True:
+            print("\nCategories:")
+            print("n: Create New Category")
+            for idx, value in self.categories.items():
+                print(f"{idx}: {value}")
+            print("Ctrl + C to exit")
+            choice = input(f"Assign a category for '{description}': ")
+
+            if choice == 'n':
+                new_category = input("Enter new category name: ")
+                if not self.categories:
+                    new_key = '1'
+                else:
+                    new_key = str(max(map(int, self.categories.keys())) + 1)
+                self.categories[new_key] = new_category
+                self.description_to_category[description] = new_category
+            elif choice in self.categories:
+                self.description_to_category[description] = self.categories[choice]
+            else:
+                print("Invalid choice. Please try again.")
+                continue
+            break
+
+
+
+class TransactionProcessor:
+    def __init__(self, file_reader, category_manager):
+        self.file_reader = file_reader
+        self.category_manager = category_manager
+
+    @staticmethod
+    def process_description(description):
+        words = description.split()
+        processed_description = ''
+        for word in words:
+            if len(processed_description + word) >= 15 and len(processed_description.split()) >= 2:
+                break
+            processed_description += (word + ' ')
+        return processed_description.strip() or description
+
+
+
+class CSVExporter:
+    @staticmethod
+    def export_to_csv(data, file_name):
+        data.to_csv(file_name, index=False)
+
+
+class DatabaseManager:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.csv_file_path = os.path.join(folder_path, 'transactions.csv')
+        self.json_file_path = os.path.join(folder_path, 'categories.json')
+        self.initialize_database()
+
+    def initialize_database(self):
+        if not os.path.exists(self.csv_file_path):
+            pd.DataFrame(columns=['date', 'description', 'amount']).to_csv(self.csv_file_path, index=False)
+        if not os.path.exists(self.json_file_path):
+            with open(self.json_file_path, 'w') as file:
+                json.dump({}, file)
+
+    def load_transactions(self):
+        return pd.read_csv(self.csv_file_path)
+
+    def save_transactions(self, df):
+        df.to_csv(self.csv_file_path, index=False)
+
+    def load_category_associations(self):
+        try:
+            with open(self.json_file_path, 'r') as file:
+                categories_to_descriptions = json.load(file)
+
+            descriptions_to_categories = {}
+            for cat, descriptions in categories_to_descriptions.items():
+                for description in descriptions:
+                    descriptions_to_categories[description] = cat
+            return descriptions_to_categories
+        except FileNotFoundError:
+            return {}
+
+    def save_category_associations(self, description_to_category):
+        category_to_descriptions = {}
+        for description, cat in description_to_category.items():
+            if cat not in category_to_descriptions:
+                category_to_descriptions[cat] = [description]
+            else:
+                category_to_descriptions[cat].append(description)
+
+        with open(self.json_file_path, 'w') as file:
+            json.dump(category_to_descriptions, file, indent=4)
--- src/tools/main.py
+++ src/tools/main.py
@@ -1,3 +1,116 @@
-from expense_checker.tools import add
+import matplotlib.pyplot as plt
+import os
 
-print(add(2,3))
+from expense_checker.tools import *
+
+def add_new_transactions(database_manager, file_path):
+    file_reader = ExcelFileReader(file_path)
+    new_transactions = file_reader.read_and_filter_data()
+    existing_transactions = database_manager.load_transactions()
+    combined_transactions = pd.concat([existing_transactions, new_transactions], ignore_index=True)
+    database_manager.save_transactions(combined_transactions)
+
+
+def categorize_transactions(database_manager):
+    transactions = database_manager.load_transactions()
+    categories = database_manager.load_category_associations()
+    category_manager = CategoryManager(categories)
+
+    try:
+        for index, row in transactions.iterrows():
+            if pd.isna(row['category']):
+                description = TransactionProcessor.process_description(str(row['description']))
+                category = category_manager.get_category_for_description(description)
+                transactions.at[index, 'category'] = category
+                database_manager.save_category_associations(category_manager.description_to_category)
+    except KeyboardInterrupt as e:
+        print("shutting down...")
+
+
+def get_categorized_transactions(database_manager):
+    transactions = database_manager.load_transactions()
+    categories = database_manager.load_category_associations()
+    category_manager = CategoryManager(categories)
+
+    transactions_with_categories = transactions[['date', 'description', 'amount']].copy()
+
+    def categorize(description):
+        desc = TransactionProcessor.process_description(str(description))
+        return category_manager.get_category_for_description(desc)
+
+    transactions_with_categories['categories'] = transactions_with_categories['description'].apply(categorize)
+
+    # Convert 'date' to datetime and extract the year and month
+    transactions_with_categories['date'] = pd.to_datetime(transactions_with_categories['date'])
+    transactions_with_categories['year_month'] = transactions_with_categories['date'].dt.to_period('M')
+
+   # Group by 'year_month' and 'categories', and sum only the 'amount' column
+    monthly_expenses = transactions_with_categories.groupby(['year_month', 'categories'])['amount'].sum().reset_index()
+    return monthly_expenses
+
+
+
+def export_transactions(database_manager, file_path):
+    monthly_expenses = get_categorized_transactions(database_manager)
+    monthly_expenses.to_excel(file_path, index=False)
+
+
+def visualize(database_manager):
+    df = get_categorized_transactions(database_manager)
+    df['amount'] = df['amount'] * -1
+
+    # Create pivot table
+    pivot_df = df.pivot_table(index='year_month', columns='categories', values='amount', fill_value=0)
+
+    # Calculate average expenses for each category and sort columns based on this
+    average_expenses = pivot_df.mean().sort_values(ascending=False)
+    pivot_df = pivot_df[average_expenses.index]
+
+    # Add 'average' as a new row in the pivot table
+    pivot_df.loc['Average'] = average_expenses
+
+    # Plot a stacked bar chart
+    pivot_df.plot(kind='bar', stacked=True, figsize=(10, 7))
+
+    plt.title('Monthly Expenses by Category')
+    plt.xlabel('Month')
+    plt.ylabel('Amount')
+    plt.legend(title='Category')
+    plt.show()
+
+
+def main():
+    folder_path = input("Enter the path to the database folder: ")
+
+    if not os.path.exists(folder_path):
+        create_folder = input(f"The folder '{folder_path}' does not exist. Create it? (y/n): ")
+        if create_folder.lower() == 'y':
+            os.makedirs(folder_path)
+        else:
+            print("Operation cancelled.")
+            return
+
+    database_manager = DatabaseManager(folder_path)
+
+    print("Select an option:")
+    print("1: Add new expenses")
+    print("2: Categorize expenses")
+    print("3: Export expenses")
+    print("4: Visualize expenses")
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        file_path = input("Enter the path to the Excel file: ")
+        add_new_transactions(database_manager, file_path)
+    elif choice == '2':
+        categorize_transactions(database_manager)
+    elif choice == '3':
+        file_path = input("Enter the path to the csv file: ")
+        export_transactions(database_manager, file_path)
+    elif choice == '4':
+        visualize(database_manager)
+    else:
+        print("Invalid choice.")
+
+if __name__ == "__main__":
+    main()
